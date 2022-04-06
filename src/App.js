169fa46f2ed5e3f353b6f35509d5ad3a115ba61f
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-04-06 17:50:15 -0400

    Conserta o lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+
 class App extends React.Component {
   render() {
     return (
