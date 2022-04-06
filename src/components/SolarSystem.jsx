2d28f26e5265d1a68a4c105546078a202c56819b
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-04-06 17:37:40 -0400

    Adiciona Requisito 6 - importei o arquivo planets.js, utilizei o map e utilizei 2 parametros, em seguida chamei o componente PlanetCard, e passei o segundo parametro com a key e as props do PlanetCard,adicionando o primeiro parametro p

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <>
-        <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
-      </>
+        {
+          planets.map((p, i) => (<PlanetCard
+            key={ i }
+            planetName={ p.name }
+            planetImage={ p.image }
+          />))
+        }
+      </div>
     );
   }
 }
